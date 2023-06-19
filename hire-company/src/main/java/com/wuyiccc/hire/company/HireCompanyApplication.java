2fb7cdfb58990a3a207067a55c34cc3f2e33c8ae
wuyiccc <3406324191@qq.com>
2023-06-19 23:05:47 +0800

    feat: web基础项目初始化

---
+++ hire-company/src/main/java/com/wuyiccc/hire/company/HireCompanyApplication.java
@@ -0,0 +1,15 @@
+package com.wuyiccc.hire.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyiccc
+ * @date 2023/6/19 23:02
+ */
+@SpringBootApplication
+public class HireCompanyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HireCompanyApplication.class, args);
+    }
+}
---
+++ hire-user/src/main/java/com/wuyiccc/hire/user/HireUserApplication.java
@@ -0,0 +1,16 @@
+package com.wuyiccc.hire.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuyiccc
+ * @date 2023/6/19 23:01
+ */
+@SpringBootApplication
+public class HireUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireUserApplication.class, args);
+    }
+}
